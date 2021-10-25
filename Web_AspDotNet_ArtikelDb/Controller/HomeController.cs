1e275936f09044564c83031098e63136cf052635
Thorsten Diederichs <DiederichsT@bfwob.de>
2021-10-25 15:44:16 +0200

    Projektdateien hinzufügen.

---
+++ Web_AspDotNet_ArtikelDb/Controller/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web_AspDotNet_ArtikelDb.Controller
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
